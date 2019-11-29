ffe8e12844d666bbe79e5624d226c6bd8db8e9c0
Mikey Epps <55078570+meppps@users.noreply.github.com>
2019-11-29 14:05:21 -0800

    Outputs candidates and results

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,23 +1,28 @@
 import os
 import csv
+from collections import Counter
 
 # Init stuff 
 voteCount = 0
 votes = []
 candidates = []
-results = {}
-results["iphone 5S"] = 2013
+results = []
+winner = 'undetermined'
 
 def output():
     print(f"""
         Election Results
-        -------------------------
+        -----------------------------------
         Total Votes: {voteCount}
-        Candidates :{results}
+        -----------------------------------
+        {candidates[0]}: {candPercents[0]}%  {candResults[0]}
+        {candidates[1]}: {candPercents[1]}%  {candResults[1]}
+        {candidates[2]}: {candPercents[2]}%  {candResults[2]}
+        {candidates[3]}: {candPercents[3]}%  {candResults[3]}
         -------------------------
-
+        Winner: {winner}
         """
-    ,width=1)
+    )
 
 # Open and read csv
 with open('election_data.csv', newline="") as csvfile:
@@ -26,18 +31,25 @@ with open('election_data.csv', newline="") as csvfile:
     # Skip headers
     next(csvreader, None)
 
-    candVotes = []
+    
 
     for rows in csvreader:
         voteCount += 1
-        candidate = rows[2]
-        votes.append(candidate)
 
-        if candidate not in candidates:
-            results[candidate] = candVotes
-        elif candidate in candidates:
-            results[candidate] = candVotes + 1
+        candidateVote = rows[2]
+        votes.append(candidateVote)
 
-    # output()
+        # if candidate not in candidates:
+            # results[candidate] = candVotes
+        # elif candidate in candidates:
 
-    print(results)
+        
+
+    
+    candidates = list(Counter(votes).keys()) # equals to list(set(votes))
+    candResults = list(Counter(votes).values()) # counts the elements' frequency
+    candPercents = [(num/voteCount) * 100 for num in candResults]
+    winner = max(candidates)
+
+    output()
+    # print(Counter(votes))
